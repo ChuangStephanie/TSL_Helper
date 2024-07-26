472aabb5db38890680fe9cff0e48cc4bae0d30a7
ChuangStephanie <stephanie231951@yahoo.com>
2024-07-26 17:32:06 -0500

    added label to shipment.jsx

--- src/components/Shipment.jsx
+++ src/components/Shipment.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Box, TextField } from "@mui/material";
 
-export default function Shipment(){
-
-    return(
-        <>
-            <h1>Shipment Labels</h1>
-        </>
-    );
+export default function Shipment() {
+  return (
+    <>
+      <h1>Shipment Pallet Labels</h1>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+      >
+        <TextField
+          id="orderNum"
+          label="Order #"
+          multiline
+          variant="filled"
+          color="primary"
+        ></TextField>
+      </Box>
+    </>
+  );
 }
