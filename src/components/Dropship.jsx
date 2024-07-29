545e6504ea5268c3efb66d8754328b7b09a75b28
ChuangStephanie <stephanie231951@yahoo.com>
2024-07-29 10:41:57 -0500

    added upload button to dropship

--- src/components/Dropship.jsx
+++ src/components/Dropship.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Box, Button } from "@mui/material";
+import { CloudUploadRounded } from "@mui/icons-material";
 
-export default function Dropship(){
+export default function Dropship() {
+  const handleFileUpload = () => {
+    //upload onto api
+  };
 
-    return(
-        <>
-            <h1>Dropship Labels</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>Dropship Labels</h1>
+      <Box className="upload">
+        <Button
+          component="label"
+          variant="contained"
+          startIcon={<CloudUploadRounded />}
+        >
+          Upload Files
+          <input type="file" hidden onChange={handleFileUpload}></input>
+        </Button>
+      </Box>
+    </>
+  );
 }
