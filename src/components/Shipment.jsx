1ac9b765e571416803949264c37e2a7cf87a9d3d
ChuangStephanie <stephanie231951@yahoo.com>
2024-07-29 10:09:50 -0500

    added form to shipment labels

--- src/components/Shipment.jsx
+++ src/components/Shipment.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, inputLabelClasses, TextField, Typography } from "@mui/material";
 
 export default function Shipment() {
   return (
@@ -10,14 +10,206 @@ export default function Shipment() {
         sx={{
           "& .MuiTextField-root": { m: 1, width: "25ch" },
         }}
+        className="form"
       >
+        <Typography variant="subtitle1">Order #</Typography>
         <TextField
           id="orderNum"
-          label="Order #"
+          label="Order #:"
           multiline
           variant="filled"
           color="primary"
+          className="textfield"
+          InputLabelProps={{
+            sx: {
+              color: "gray",
+              [`&.${inputLabelClasses.shrink}`]: {
+                color: "primary",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              color: "gray",
+            },
+          }}
         ></TextField>
+        <Box>
+          <Typography variant="subtitle1">Destination</Typography>
+          <TextField
+            id="address"
+            label="Address:"
+            multiline
+            variant="filled"
+            color="primary"
+            className="textfield"
+            InputLabelProps={{
+              sx: {
+                color: "gray",
+                [`&.${inputLabelClasses.shrink}`]: {
+                  color: "primary",
+                },
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-input": {
+                color: "gray",
+              },
+            }}
+          ></TextField>
+          <TextField
+            id="city"
+            label="City:"
+            multiline
+            variant="filled"
+            color="primary"
+            className="textfield"
+            InputLabelProps={{
+              sx: {
+                color: "gray",
+                [`&.${inputLabelClasses.shrink}`]: {
+                  color: "primary",
+                },
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-input": {
+                color: "gray",
+              },
+            }}
+          ></TextField>
+          <TextField
+            id="state"
+            label="State:"
+            multiline
+            variant="filled"
+            color="primary"
+            className="textfield"
+            InputLabelProps={{
+              sx: {
+                color: "gray",
+                [`&.${inputLabelClasses.shrink}`]: {
+                  color: "primary",
+                },
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-input": {
+                color: "gray",
+              },
+            }}
+          ></TextField>
+          <TextField
+            id="zip"
+            label="Zip Code:"
+            multiline
+            variant="filled"
+            color="primary"
+            className="textfield"
+            InputLabelProps={{
+              sx: {
+                color: "gray",
+                [`&.${inputLabelClasses.shrink}`]: {
+                  color: "primary",
+                },
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-input": {
+                color: "gray",
+              },
+            }}
+          ></TextField>
+        </Box>
+        <Typography variant="subtitle1">Shipping Carrier</Typography>
+        <TextField
+          id="carrier"
+          label="Shipping Carrier:"
+          multiline
+          variant="filled"
+          color="primary"
+          className="textfield"
+          InputLabelProps={{
+            sx: {
+              color: "gray",
+              [`&.${inputLabelClasses.shrink}`]: {
+                color: "primary",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              color: "gray",
+            },
+          }}
+        ></TextField>
+        <Typography variant="subtitle1">Item(s)</Typography>
+        <TextField
+          id="item"
+          label="Item(s):"
+          multiline
+          variant="filled"
+          color="primary"
+          className="textfield"
+          InputLabelProps={{
+            sx: {
+              color: "gray",
+              [`&.${inputLabelClasses.shrink}`]: {
+                color: "primary",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              color: "gray",
+            },
+          }}
+        ></TextField>
+        <Typography variant="subtitle1">Total Pallets</Typography>
+        <TextField
+          id="pallets"
+          label="Total Pallets:"
+          multiline
+          variant="filled"
+          color="primary"
+          className="textfield"
+          InputLabelProps={{
+            sx: {
+              color: "gray",
+              [`&.${inputLabelClasses.shrink}`]: {
+                color: "primary",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              color: "gray",
+            },
+          }}
+        ></TextField>
+        <Typography variant="subtitle1">Scheduled Ship Date</Typography>
+        <TextField
+          id="shipdate"
+          label="Ship Date:"
+          multiline
+          variant="filled"
+          color="primary"
+          className="textfield"
+          InputLabelProps={{
+            sx: {
+              color: "gray",
+              [`&.${inputLabelClasses.shrink}`]: {
+                color: "primary",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              color: "gray",
+            },
+          }}
+        ></TextField>
+        <Button type="submit">Submit</Button>
       </Box>
     </>
   );
